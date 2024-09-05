3b7990e746516854ea2013c8cc3c51495dfe7ac0
ramisa <92804590+ramisa7@users.noreply.github.com>
2024-09-04 23:16:20 -0400

    Landing page skeleton & Layout base touch-up

---
+++ client/src/app/head.js
@@ -0,0 +1,14 @@
+// src/app/head.js
+
+export default function Head() {
+    return (
+      <>
+        <title>REPRESSO</title>
+        <meta name="description" content="REPRESSO - A modern clothing brand" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </>
+    );
+  }
+  
